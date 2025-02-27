cca2d5b41565071f3b5da0694151acbab6509884
Sahil Dhingani <sahildhingani51@gmail.com>
2025-02-27 14:15:04 +0530

    initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import FirstPage from './Pages/Login'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <FirstPage/>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/Pages/Login.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function FirstPage() {
+
+  return (
+    <>
+    <div className='h-screen w-screen ' >
+    </div>
+    </>
+  )
+}
+
+export default FirstPage
